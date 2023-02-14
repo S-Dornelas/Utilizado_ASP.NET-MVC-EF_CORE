f01756f9c68ba07b6d03ad4e2c9fc1e0671eab35
Sandro Dornelas <sandrodornelas.sd@gmail.com>
2023-02-14 10:57:01 -0300

    Add files via upload

---
+++ MinhaApi/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public List<TodoModel> Get([FromServices] AppDbContext context)
+        {
+            return context.Todos.ToList();
+        }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post(
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x=>x.Id == id);
+            if(model == null)
+                return null;
+            
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+         [HttpDelete("/{id:int}")]
+        public TodoModel Delete(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x=>x.Id == id);
+            context.Todos.Remove(model);
+            context.SaveChanges();
+            return model;
+        }
+    }
+}
